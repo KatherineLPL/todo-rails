aa721b915e1bdf0b2cd210ee93d99d8ed6a35dde
Katherine Evans <katherine@launchpadlab.com>
2021-11-15 12:54:08 -0600

    chapter 2

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::TodosController < ApplicationController
-    include RailsUtil::JsonHelper
 
-    def index 
-        todos = Todo.in_order_by_creation
-        json_with todos
-    end
+    def index
+    todos = Todo.all
+    json_with todos
+  end
 
+  def create
+    todo = create_todo_service(todo_params).call
+    json_with todo
+  end
+
+  private
+
+  def todo_params
+    @todo_params ||= TodoDecanter.decant(params[:todo])
+  end
+
+  def create_todo_service(args = {})
+    CreateTodos.new(args)
+  end
 end
